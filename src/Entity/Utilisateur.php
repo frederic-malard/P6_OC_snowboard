0c6bc484206603473d4dc5dfa39f843ac734a8e0
travail <travail@macbook-pro-de-frederic.home>
2019-11-30 15:50:34 +0100

    connexion, déconnexion et inscription, non finalisé

--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -5,10 +5,12 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UtilisateurRepository")
+ * @UniqueEntity(fields={"login"}, message="Ce login est déjà pris.")
  */
 class Utilisateur implements UserInterface
 {
