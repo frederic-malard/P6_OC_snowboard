a46ae7a160c60aa66b54b84239386327fa6af434
travail <travail@macbook-pro-de-frederic.home>
2019-12-18 00:44:22 +0100

    accueil : recherche textuelle et filtre groupes gérés

--- public/js/gestionFigures.js
+++ public/js/gestionFigures.js
@@ -2,8 +2,8 @@ $(function(){
     $corps = $("#corps");
     $tricks = $('#tricks');
 
-    $figuresAffichees = $('.d-block');
-    $figuresMasquees = $('.d-none');
+    $figuresAffichees = $('.figure.d-block');
+    $figuresMasquees = $('.figure.d-none');
     $figuresTotal = $('.figure');
 
     $portionAffichage = 15;
@@ -14,6 +14,7 @@ $(function(){
 
         $boutonAfficher = $('<button></button>');
         $boutonAfficher.text("Afficher plus");
+        $boutonAfficher.attr("id", "boutonAfficherPlus");
 
         $corps.append($pBouton);
         $pBouton.append($boutonAfficher);
@@ -37,8 +38,8 @@ $(function(){
                 }
             });
 
-            $figuresAffichees = $('.d-block');
-            $figuresMasquees = $('.d-none');
+            $figuresAffichees = $('.figure.d-block');
+            $figuresMasquees = $('.figure.d-none');
 
             // retire le bouton "afficher plus" si toutes les figures sont affichées
             if ($figuresMasquees.length == 0) {
@@ -67,52 +68,7 @@ $(function(){
 
                 $flecheVersHautAffichee = true;
             }
-
-            //$gererModification();
-            $gererSuppression();
         });
         
     }
-
-
-
-    // confirmation modification figure
-
-    /*$gererModification = function(){
-        $lienModifier = $("#modifierFigure");
-
-        $lienModifier.click(function(event){
-            if (confirm("Etes vous sur de vouloir modifier la figure ?")){
-                window.location.replace($pathModifier);
-            }
-        });
-    };
-
-    $gererModification();*/
-
-
-
-    // confirmation suppression figure
-
-    $gererSuppression = function(){
-        /*$liensSupprimer = $('.supprimerFigure');
-    
-        $liensSupprimer.each(function(cle, lien){
-            $lien = jQuery(lien);
-            
-            $lien.click(function(event){
-                event.preventDefault();
-
-                $lien = jQuery(event.target.parentNode);
-                $slugFigure = $lien.attr('id');
-                console.log($slugFigure);
-                
-                if (confirm("Etes vous sur de vouloir supprimer la figure ?")){
-                    window.location.replace($pathSupprimer.substring(0, $pathSupprimer.length - 6) + $slugFigure);
-                }
-            });
-        });*/
-    };
-
-    $gererSuppression();
 });
--- public/js/rechercheFigure.js
+++ public/js/rechercheFigure.js
@@ -1,5 +1,20 @@
 $(function(){
 
+    // préparation variables
+
+    $inputGroupes = $(".filtreGroupe");
+    $champsRecherche = $("#recherche");
+    $figures = $(".figure");
+
+    // fonction pour masquer le bouton afficher plus
+
+    $masquerBouton = function(){
+        $bouton = $("#boutonAfficherPlus");
+        if (! $bouton.hasClass("d-none")) {
+            $boutonAfficher.addClass("d-none");
+        }
+    };
+
     // afficher-masquer la zone de recherche tri filtre
     
     $blockRecherche = $("#blockRecherche");
@@ -15,25 +30,93 @@ $(function(){
         $blockRecherche.toggle();
     });
 
-    // gestion de la recherche textuelle
+    // tout afficher
 
-    $champsRecherche = $("#recherche");
+    $toutAfficher = function(){
+        $(".figure").removeClass("d-none");
+        $(".figure").addClass("d-block");
+    };
 
-    $figures = $(".figure");
+    // masquer ce qui ne correspond pas à la recherche textuelle (sauf si recherche vide)
 
-    $champsRecherche.keyup(function(){
+    $masquerRecherche = function(){
         $aChercher = $champsRecherche.val();
         $figures.each(function(index, element){
-            $nom = jQuery(element).children().first().children().last().children().last().text();
-            if ($nom.indexOf($aChercher) >= 0){
-                console.log("montre");
-                jQuery(element).removeClass("d-none");
-                jQuery(element).addClass("d-block");
+            $nom = jQuery(element).children().eq(1).children().last().children().last().text().toLowerCase();
+            if ($nom.toLowerCase().indexOf($aChercher) >= 0){
+                // jQuery(element).removeClass("d-none");
+                // jQuery(element).addClass("d-block");
             } else {
-                console.log("cache");
                 jQuery(element).addClass("d-none");
                 jQuery(element).removeClass("d-block");
             }
         });
+    };
+
+    // masquer ce qui ne correspond pas aux groupes sélectionnés (sauf si tout déselectionné)
+
+    $masquerGroupes = function(){
+        nomsGroupesCoches = [];
+        nomsGroupesDecoches = [];
+        $inputGroupes.each(function(cle, element){
+            $inputGroupe = jQuery(element);
+            if ($inputGroupe.is(":checked")) {
+                nomsGroupesCoches.push($inputGroupe.attr("id"));
+            } else {
+                nomsGroupesDecoches.push($inputGroupe.attr("id"));
+            }
+        });
+        $nombreCoches = nomsGroupesCoches.length;
+        if ($nombreCoches == 0) {
+            $(".figure").each(function(cle, element){
+                $figure = jQuery(element);
+                // $figure.show();
+            });
+        } else {
+            $.each(nomsGroupesCoches, function(cle, element){
+                nomGroupeCoche = element;
+                $('.'+nomGroupeCoche).each(function(cle, element){
+                    $figureCochee = jQuery(element);
+                    // $figureCochee.show();
+                    // $figureCochee.addClass("d-block");
+                    // $figureCochee.removeClass("d-none");
+                });
+            });
+            $.each(nomsGroupesDecoches, function(cle, element){
+                nomGroupeDecoche = element;
+                $('.'+nomGroupeDecoche).each(function(cle, element){
+                    $figureDecochee = jQuery(element);
+                    // $figureDecochee.hide();
+                    $figureDecochee.addClass("d-none");
+                    $figureDecochee.removeClass("d-block");
+                });
+            });
+        }
+    };
+
+    // actions à faire à chaque événement
+
+    $actions = function(){
+        $toutAfficher();
+        $masquerRecherche();
+        $masquerGroupes();
+        $masquerBouton();
+    };
+
+    // gestion de la recherche textuelle
+
+    $champsRecherche.keyup(function(){
+        $actions();
     });
+
+    // filtrer par groupes
+
+    $inputGroupes.each(function(cle, element){
+        $inputGroupe = jQuery(element);
+        $inputGroupe.click(function(event){
+            $actions();
+        });
+    });
+
+
 });
