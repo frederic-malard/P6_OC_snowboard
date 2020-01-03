2fac29fb5b1ec4cc762d367cc10e841d95fe4651
travail <travail@macbook-pro-de-frederic.home>
2020-01-03 22:33:45 +0100

    Fixes #85 amélioration fixtures : avatard, plus de commentaires, certains coms signalés

--- src/Migrations/Version20200103213007.php
+++ src/Migrations/Version20200103213007.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200102114610 extends AbstractMigration
+final class Version20200103213007 extends AbstractMigration
 {
     public function getDescription() : string
     {
